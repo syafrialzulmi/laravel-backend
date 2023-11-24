846075e0495faa160f23c5e5a701adf5d88c4fc7
Syafrial Zulmi <syafrialzulmi@gmail.com>
2023-11-25 04:12:49 +0700

    update api user

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+use App\Http\Resources\UserResource;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:100|min:3',
+            'email' => 'required|email|unique:users,email',
+            'phone' => 'required|numeric',
+            'roles' => 'required|in:USER',
+            'password' => 'required|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'phone' => $validatedData['phone'],
+            'roles' => 'USER',
+            'password' => Hash::make($validatedData['password']),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken();
+
+        return response()->json([
+            'access_token' =>$token,
+            'user' => UserResource::make($user),
+        ]);
+    }
+
+    public function login (Request $request)
+    {
+        $validatedData = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $validatedData['email'])->first;
+
+        if (!$user) {
+            return response()->json([
+                'message' => 'User not found',
+            ], 401);
+        }
+
+        if (!Hash::check($validatedData['password'], $user->password)) {
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken();
+
+        return response()->json([
+            'access_token' =>$token,
+            'user' => UserResource::make($user),
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Logout success',
+        ]);
+    }
+}
